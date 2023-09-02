73f538b7f2cd270d6b92ef7947d4ef97518c8edf
Alparslan <alparslanaydgn02@gmail.com>
2023-09-02 16:00:32 +0300

    Add project files.

---
+++ Infrastructure/Persistence/Concretes/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using Application.Abstractşons;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        
+        public List<Product> GetProducts()
+            => new()
+            {
+                new() { Id=Guid.NewGuid(),Name="Product 1", Price=100,Stock=10,CreateDate=DateTime.Now},
+                new() { Id=Guid.NewGuid(),Name="Product 2", Price=200,Stock=20,CreateDate=DateTime.Now},
+                new() { Id=Guid.NewGuid(),Name="Product 3", Price=300,Stock=30,CreateDate=DateTime.Now},
+                new() { Id=Guid.NewGuid(),Name="Product 4", Price=400,Stock=40,CreateDate=DateTime.Now},
+                new() { Id=Guid.NewGuid(),Name="Product 5", Price=500,Stock=50,CreateDate=DateTime.Now}
+            };
+        //{
+        //    List<Product> liste = new List<Product>();
+        //    Product product = new Product { Id = Guid.NewGuid(), Name = "product 1", Price = 100, Stock = 10 };
+        //    liste.Add(product);
+        //    return liste;
+        //    Üsttekiyle bu aynı. Üstteki daha iyi.
+        //}
+    }
+}
---
+++ Infrastructure/Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Application.Abstractşons;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
