40004e64556537bc6303f8b045438ae889a1c530
Alparslan <alparslanaydgn02@gmail.com>
2023-09-14 13:56:54 +0300

    Cors politikası için izinler verildi.

--- Presentation/WebAPI/Program.cs
+++ Presentation/WebAPI/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistenceServices();
 
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy=> 
+policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()
+)); 
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
