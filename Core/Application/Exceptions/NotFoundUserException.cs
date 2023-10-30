12e4a56be120863027e641bd06f55d1e06a3689e
Alparslan <alparslanaydgn02@gmail.com>
2023-10-30 17:06:35 +0300

    Ders 39=> Login işlemleri tamamlandı. Authantication tamamlandı. Bir sonraki iişlemlerde rol yani authorize yapılacak

---
+++ Core/Application/Exceptions/NotFoundUserException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class NotFoundUserException : Exception
+    {
+        public NotFoundUserException() : base("Kullanıcı adı veye şifre hatalı.")
+        {
+        }
+
+        public NotFoundUserException(string? message) : base(message)
+        {
+        }
+
+        protected NotFoundUserException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Core/Application/Features/Commands/AppUser/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppUsers = Domain.Entities.Identity.AppUser;
+
+namespace Application.Features.Commands.AppUser.LoginUser
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommandRequest, LoginUserCommandResponse>
+    {
+        readonly UserManager<AppUsers> _userManager;
+        readonly SignInManager<AppUsers> _signInManager;
+
+        public LoginUserCommandHandler(UserManager<AppUsers> userManager, SignInManager<AppUsers> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<LoginUserCommandResponse> Handle(LoginUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            AppUsers user = await _userManager.FindByNameAsync(request.userNameorEmail);
+            if (user == null)
+                user = await _userManager.FindByEmailAsync(request.userNameorEmail);
+
+            if (user == null)
+                throw new NotFoundUserException("Kullanıcı adı veya şifre hatalı...");
+
+            SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+            if (result.Succeeded)
+            {
+                //yetkiler belirlenecek
+            }
+
+            return new();
+        }
+    }
+}
