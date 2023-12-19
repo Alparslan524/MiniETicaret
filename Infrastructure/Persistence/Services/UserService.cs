8261b3f60f5fbc27828126a560d30da3269364e4
Alparslan <alparslanaydgn02@gmail.com>
2023-12-19 12:34:38 +0300

    Tüm kullanıcılar listelendi ve rol atama işlemleri tamamlandı.

--- Infrastructure/Persistence/Services/UserService.cs
+++ Infrastructure/Persistence/Services/UserService.cs
@@ -6,6 +6,7 @@ using Application.Helpers;
 using Azure.Core;
 using Domain.Entities.Identity;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,6 @@ namespace Persistence.Services
     public class UserService : IUserService
     {
         readonly UserManager<AppUser> _userManager;
-
         public UserService(UserManager<AppUser> userManager)
         {
             _userManager = userManager;
@@ -68,6 +68,47 @@ namespace Persistence.Services
                 else
                     throw new PasswordChangeFailedException();
             }
+        }
+
+        public async Task<List<ListUser>> GetAllUsersAsync(int page, int size)
+        {
+            var users = await _userManager.Users
+                .Skip(page * size)
+                .Take(size)
+                .ToListAsync();
+
+            return users.Select(user => new ListUser
+            {
+                Id = user.Id,
+                Email = user.Email,
+                NameSurname = user.NameSurname,
+                TwoFactorEnabled = user.TwoFactorEnabled,
+                UserName = user.UserName,
+            }).ToList();
+        }
+
+
+        public int TotalUsersCount => _userManager.Users.Count();
+        public async Task AssignRoleToUserAsync(string userId, string[] roles)
+        {
+            AppUser user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                await _userManager.RemoveFromRolesAsync(user, userRoles);
+                await _userManager.AddToRolesAsync(user, roles);
+            }
+        }
+
+        public async Task<string[]> GetRolesToUserAsync(string userId)
+        {
+            AppUser user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                return userRoles.ToArray();
+            }
+            return new string[] { };
         }
     }
 }
--- Presentation/WebAPI/Controllers/UsersController.cs
+++ Presentation/WebAPI/Controllers/UsersController.cs
@@ -1,9 +1,15 @@
 ﻿using Application.Abstractions.Services;
+using Application.CustomAttributes;
+using Application.Enums;
+using Application.Features.Commands.AppUser.AssignRoleToUser;
 using Application.Features.Commands.AppUser.CreateUser;
 using Application.Features.Commands.AppUser.GoogleLoginUser;
 using Application.Features.Commands.AppUser.LoginUser;
 using Application.Features.Commands.AppUser.UpdatePassword;
+using Application.Features.Queries.AppUser.GetAllUsers;
+using Application.Features.Queries.AppUser.GetRolesToUser;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -35,5 +41,36 @@ namespace WebAPI.Controllers
             UpdatePasswordCommandResponse response = await _mediator.Send(request);
             return Ok();
         }
+
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = "Admin")]
+        [AuthorizeDefinition(ActionType = ActionType.Reading, Definition = "Get All Users", Menu = "Users")]
+        public async Task<IActionResult> GetAllUsers([FromQuery] GetAllUsersQueryRequest request)
+        {
+            GetAllUsersQueryResponse response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+
+        [HttpPost("assign-role-to-user")]
+        [Authorize(AuthenticationSchemes = "Admin")]
+        [AuthorizeDefinition(ActionType = ActionType.Writing, Definition = "Assign Role To User", Menu = "Users")]
+        public async Task<IActionResult> AssignRoleToUser([FromBody] AssignRoleToUserCommandRequest request)
+        {
+            AssignRoleToUserCommandResponse response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+
+        [HttpGet("get-roles-to-user/{UserID}")]
+        [Authorize(AuthenticationSchemes = "Admin")]
+        [AuthorizeDefinition(ActionType = ActionType.Reading, Definition = "Get Roles To User", Menu = "Users")]
+        public async Task<IActionResult> GetRolesToUser([FromRoute] GetRolesToUserQueryRequest request)
+        {
+            GetRolesToUserQueryResponse response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
     }
 }
